5e3969a000aab8543eb3ad59abf1738d99a5d10d
Liam Davidson <ldavidson@lcs.on.ca>
2019-11-13 19:18:03 -0500

    Initial Commit

---
+++ TrafficSegues/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrafficSegues
+//
+//  Created by Davidson, Liam on 2019-11-13.
+//  Copyright © 2019 Davidson, Liam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

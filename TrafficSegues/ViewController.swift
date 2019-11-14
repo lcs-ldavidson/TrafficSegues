a0cd2249608db31dde8ab9abe76b2e1c1ec55844
Liam Davidson <ldavidson@lcs.on.ca>
2019-11-13 20:18:01 -0500

    Got an example of passing info between scenes.

--- TrafficSegues/ViewController.swift
+++ TrafficSegues/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -17,6 +21,12 @@ class ViewController: UIViewController {
 
     @IBAction func unwindToRed(unwindSegue: UIStoryboardSegue) {
         
+    }
+    
+    //get ready to segue to the yellow view controller
+    //is called automatically when a segue is about to happen
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        segue.destination.navigationItem.title = textField.text
     }
 
 }
